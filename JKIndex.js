c44498741403a953efdc0f4e94450002d5fc85c2
Nobleman444 <ruiz.eric6@gmail.com>
2022-03-14 21:41:04 -0500

    Update JKIndex.js

--- JKIndex.js
+++ JKIndex.js
@@ -1,25 +1,25 @@
 Object.defineProperty(globalThis, "Definer", {
-  configurable: true, enumerable: false, writable: true,
-  value: new Proxy({t: globalThis}, {
-    has(tar, nam) {},
-    get(tar, nam) {},
-    set(tar, nam, val) {
-      
-    },
-    
-    //apply(tar, thi, arg) {},
-    //construct(tar, arg) {},
-    
-    defineProperty(tar, nam, des) {},
-    deleteProperty(tar, nam) {},
-    
-    ownKeys(tar) {},
-    getOwnPropertyDescriptor(tar, nam) {},
-    
-    getPrototypeOf(tar) {},
-    setPrototypeOf(tar, pro) {},
-    
-    isExtensible(tar) {},
-    preventExtensions(tar) {}
-  })
+    configurable: true, enumerable: false, writable: true,
+    value: new Proxy({t: globalThis}, {
+        has(tar, nam) {},
+        get(tar, nam) {},
+        set(tar, nam, val) {
+            
+        },
+        
+        //apply(tar, thi, arg) {},
+        //construct(tar, arg) {},
+        
+        defineProperty(tar, nam, des) {},
+        deleteProperty(tar, nam) {},
+        
+        ownKeys(tar) {},
+        getOwnPropertyDescriptor(tar, nam) {},
+        
+        getPrototypeOf(tar) {},
+        setPrototypeOf(tar, pro) {},
+        
+        isExtensible(tar) {},
+        preventExtensions(tar) {}
+    })
 });
