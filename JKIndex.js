cb9c812173916e8f53152adf4c66c1b35883fd25
Nobleman444 <ruiz.eric6@gmail.com>
2022-03-15 05:59:56 -0500

    Update JKIndex.js

--- JKIndex.js
+++ JKIndex.js
@@ -1,10 +1,19 @@
 Object.defineProperty(globalThis, "Definer", {
     configurable: true, enumerable: false, writable: true,
-    value: new Proxy({t: globalThis}, {
+    value: new Proxy({t: globalThis, n: "Definer", get rx() {return this.n in this.t;}}, {
         has(tar, nam) {},
         get(tar, nam) {},
         set(tar, nam, val) {
-            
+            if (nam.toLowerCase() == "target") {
+                switch (typeof val) {
+                    case "function":
+                    case "object": tar.t = val; return;
+                    default:
+                        if (val in tar.t)
+                }
+            } else if (name.toLowerCase() == "name") {
+                
+            }
         },
         
         //apply(tar, thi, arg) {},
