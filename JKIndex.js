15d193b700986ca24517b1d0d2aa996f86fe2001
Nobleman444 <ruiz.eric6@gmail.com>
2022-03-14 20:42:24 -0500

    Update JKIndex.js

--- JKIndex.js
+++ JKIndex.js
@@ -1,1 +1,8 @@
-
+Object.defineProperty(globalThis, "Definer", {
+  configurable: true, enumerable: false, writable: true,
+  value: new Proxy({t: globalThis}, {
+    has(tar, nam) {},
+    get(tar, nam) {},
+    set(tar, nam, val) {}
+  })
+});
