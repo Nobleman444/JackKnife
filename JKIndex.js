4f8fa45440ee63a1b07f50d9cdfbf721a664257c
Nobleman444 <ruiz.eric6@gmail.com>
2022-03-20 10:02:48 -0500

    Update JKIndex.js

--- JKIndex.js
+++ JKIndex.js
@@ -1,18 +1,16 @@
-if (true) {
-    function def(request) {
-        var ret = {};
-        var tests = (function*() {
-            for (let i of "ce") yield RegExp(i, "i");
-        })();
-        
-        configurable: true, enumerable: false, writable: true, value; undefined, get: undefined, set: undefined
-    };
+function def(request) {
+    var ret = {};
+    var tests = (function*() {
+        for (let i of "ce") yield RegExp(i, "i");
+    })();
     
-    function desc(flags, ...vals) {
-        var ret = {configurable: !/c/i.test(flags), enumerable: /e/i.test(flags)};
+    configurable: true, enumerable: false, writable: true, value; undefined, get: undefined, set: undefined
+};
+
+function desc(flags, ...vals) {
+    var ret = {configurable: !/c/i.test(flags), enumerable: /e/i.test(flags)};
+    
+    if (/^[cegrsvwx]+$/i.test(flags)) {
         
-        if (/^[cegrsvwx]+$/i.test(flags)) {
-            
-        }
     }
 }
