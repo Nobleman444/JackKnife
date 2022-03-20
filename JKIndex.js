114b2210d2277745886fa43c1ce168ed41435dfd
Nobleman444 <ruiz.eric6@gmail.com>
2022-03-20 12:40:06 -0500

    Update JKIndex.js

--- JKIndex.js
+++ JKIndex.js
@@ -1,16 +1,26 @@
-function def(request) {
-    var ret = {};
-    var tests = (function*() {
-        for (let i of "ce") yield RegExp(i, "i");
-    })();
+function definer(target, name, desc) {
+    var ret = {configurable: true, enumerable: false, get: undefined, set: undefined, value: undefined, writable: true};
     
-    configurable: true, enumerable: false, writable: true, value; undefined, get: undefined, set: undefined
-};
-
-function desc(flags, ...vals) {
-    var ret = {configurable: !/c/i.test(flags), enumerable: /e/i.test(flags)};
+    function test(rx, remove = true) {
+        var ret = rx.test(flags);
+        if (ret && remove) flags = flags.replaceAll(rx, "");
+        return ret;
+    }
     
-    if (/^[cegrsvwx]+$/i.test(flags)) {
+    ret.configurable = !test(/c/gi);
+    ret.enumerable = test(/e/gi);
+    
+    if (test(/a/gi) || test(/gs/gi, false)) {
+        let [g, s] = ["g", "s"].map(u => flags.search(RegExp(u, "gi")));
         
+        if (g == -1) {
+            ret.get = undefined;
+            ret.set = s == -1 ? undefined : specs[0];
+        } else if (s == -1) {
+            ret.get = specs[0];
+            ret.set = undefined;
+        } else {
+            [ret.get, ret.set] = (g < s ? [0, 1] : [1, 0]).map(u => specs[u]);
+        }
     }
 }
